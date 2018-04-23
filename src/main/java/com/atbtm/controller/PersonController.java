34094cec2d1379cedea3fbaafb2715d76c822f15
sm045549 <atbtmmm@gmail.com>
2018-04-23 16:54:07 -0400

    Remove unused files. Adding one non-rest controller to test how it handles the non-templated plain string return

--- src/main/java/com/atbtm/controller/PersonController.java
+++ src/main/java/com/atbtm/controller/PersonController.java
@@ -1,21 +1,19 @@
 package com.atbtm.controller;
 
 import java.util.List;
-import java.util.Map;
 
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.atbtm.domain.HttpResult;
 import com.atbtm.domain.Person;
@@ -24,7 +22,7 @@ import com.atbtm.repository.PersonRepository;
 import com.atbtm.service.PersonService;
 import com.atbtm.utils.HttpResultUtil;
 
-@Controller
+@RestController
 public class PersonController {
 	@Autowired
 	private PersonRepository pr;
@@ -35,6 +33,7 @@ public class PersonController {
 	@Autowired
 	private BfProperties bfp;
 	
+	@CrossOrigin(origins = "*")
 	@GetMapping(value="/bfProps")
 	public String getBfProps() {
 		return bfp.toString();
@@ -104,11 +103,5 @@ public class PersonController {
 	@GetMapping("/persons/desc")
 	public List<Person> getPersonsInDesc() {
 		return ps.descPersons();
-	}
-	
-	@RequestMapping("/home")
-	public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
-		model.addAttribute("name", name);
-		return "home";
 	}
 }
